a82b2acc48d7d1cac318ba076e577aa0e8bb5963
Misha Wagner <mishajw@gmail.com>
2016-05-10 17:43:07 +0100

    Initial commit

---
+++ run.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+BUILD_DIR=".build"
+OUTPUT_NAME="hash-table"
+OUTPUT_FULL="$BUILD_DIR/$OUTPUT_NAME"
+
+do_build() {
+  mkdir -p $BUILD_DIR
+  gcc ./src/main.c -o $OUTPUT_FULL
+}
+
+do_run() {
+  $OUTPUT_FULL
+}
+
+if do_build; then
+  do_run
+fi
+

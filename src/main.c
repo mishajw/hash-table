88230fe4408e165a1a6ff0fc4d52b12967dbd7d8
Misha Wagner <mishajw@gmail.com>
2016-05-10 18:17:08 +0100

    Add function for adding entry to table at location

--- src/main.c
+++ src/main.c
@@ -36,11 +36,27 @@ void set_table_size(struct table *t, unsigned int size) {
   memset(t->entries, '\0', chunk_size);
 }
 
+void add_to_table(struct table *t, struct table_entry *te, unsigned int location) {
+  if (location >= t->size) {
+    fprintf(stderr, "Location %d too large for table of size %d", location, t->size);
+    exit(1);
+  }
+
+  struct table_entry* te_current = t->entries[location];
+
+  if (!te_current) {
+    t->entries[location] = te;
+    return;
+  }
+
+  fprintf(stderr, "Collision detected. Not adding element to table.");
+}
+
 int main() {
   struct table* t = mk_table();
   set_table_size(t, 100);
 
-  printf("%d", t->size);
-  printf("%d", t->entries[51]);
+  struct table_entry* te = mk_entry();
+  add_to_table(t, te, 5);
 }
 

372d7ae74f06f5929f6812d5acb7a63744a9a13f
Misha Wagner <mishajw@gmail.com>
2016-05-10 19:00:02 +0100

    Preliminary hash function

--- src/main.c
+++ src/main.c
@@ -3,6 +3,16 @@
 #include <stdlib.h>
 #include <string.h>
 
+unsigned long hash(unsigned char *str) {
+    unsigned long hash = 5381;
+    int c;
+
+    while (c = *str++)
+        hash = ((hash << 5) + hash) + c;
+
+    return hash;
+}
+
 int main() {
   struct table* t = mk_table(100, NULL);
 
